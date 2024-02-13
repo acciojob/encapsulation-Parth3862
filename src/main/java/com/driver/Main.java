fe1d6472aabdd08e4421f4e33f6abfdeb029e870
Parth <parthudgirkar@gmail.com>
2024-02-13 15:39:44 +0530

    encapsulation commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+      // Error: Cannot access private member 'name' from outside the class
+      // obj.name = "Parth";
+
+      // Error: Cannot access private member 'name' from outside the class
+      // System.out.println(obj.name);
+      obj.setName("parth");
+      System.out.println(obj.getName());
+  }
 }
